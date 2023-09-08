d4054e5932adeabcc9e45a24dbb763e3cb633d64
MD. MEHEDI HASAN <mehedi.ofcc@gmail.com>
2023-09-08 13:54:43 +0600

    complete dashboard functionality

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProvider';
 import { toast } from 'react-hot-toast';
-import { FaChartBar, FaRegCalendarPlus, FaTable, FaTasks, FaUserPlus } from "react-icons/fa";
+import { FaChartBar, FaRegCalendarPlus, FaTable, FaTasks } from "react-icons/fa";
 import useTeamLeader from '../hooks/useTeamLeader';
 
 const MainLayout = () => {
@@ -63,7 +63,6 @@ const MainLayout = () => {
                     <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
                         {/* Sidebar content here */}
                         <li><Link to="/dashboard"><FaChartBar />Dashboard</Link></li>
-                        {isTeamLeader && <li><Link to="/add-member"><FaUserPlus />Add Member</Link></li>}
                         {!isTeamLeader && <li><Link to="/my-task"><FaTable />My Task</Link></li>}
                         <li><Link to="/all-task"><FaTasks />All Task</Link></li>
                         {isTeamLeader && <li><Link to="/create-task"><FaRegCalendarPlus />Create Task</Link></li>}
