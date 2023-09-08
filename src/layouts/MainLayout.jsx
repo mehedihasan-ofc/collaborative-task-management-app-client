237f9b160ddde6ebb1d08de6428d405c5c8fb7cb
MD. MEHEDI HASAN <mehedi.ofcc@gmail.com>
2023-09-08 09:39:44 +0600

    Added input 'MainLayout updated

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -20,47 +20,7 @@ const MainLayout = () => {
             .catch(err => console.log(err))
     }
 
-    const navOptions = <>
-        <li><Link to='/'>Home</Link></li>
-        {!user && <li><Link to='/login'>Log In</Link></li>}
-        {!user && <li><Link to='/signup'>Sign Up</Link></li>}
-        {user && <li onClick={handleLogOut}><Link>Log Out</Link></li>}
-    </>
-
     return (
-        // <>
-        //     <div className="drawer">
-        //         <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-        //         <div className="drawer-content flex flex-col">
-        //             {/* Navbar */}
-        //             <div className="w-full navbar shadow-sm">
-        //                 <div className="flex-none lg:hidden">
-        //                     <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
-        //                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-        //                     </label>
-        //                 </div>
-        //                 <div className="flex-1 px-2 mx-2">CTMA</div>
-        //                 <div className="flex-none items-center hidden lg:block">
-        //                     <ul className="menu menu-horizontal">
-        //                         {/* Navbar menu content here */}
-        //                         {navOptions}
-        //                     </ul>
-        //                 </div>
-        //             </div>
-        //             {/* Page content here */}
-        //             <Outlet />
-        //         </div>
-        //         <div className="drawer-side">
-        //             <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-        //             <ul className="menu p-4 w-80 min-h-full bg-base-200">
-        //                 {/* Sidebar content here */}
-        //                 {navOptions}
-        //             </ul>
-
-        //         </div>
-        //     </div>
-        // </>
-
         <>
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -103,9 +63,9 @@ const MainLayout = () => {
                     <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
                         {/* Sidebar content here */}
                         <li><Link to="/dashboard"><FaChartBar />Dashboard</Link></li>
-                        <li><Link to="/add-member"><FaUserPlus />Add Member</Link></li>
+                        {isTeamLeader && <li><Link to="/add-member"><FaUserPlus />Add Member</Link></li>}
                         <li><Link to="/all-task"><FaTasks />All Task</Link></li>
-                        <li><Link to="/create-task"><FaRegCalendarPlus />Create Task</Link></li>
+                        {isTeamLeader && <li><Link to="/create-task"><FaRegCalendarPlus />Create Task</Link></li>}
                     </ul>
 
                 </div>
